11730b46b2db3c54be55f2c2018b4fbfbb7dc32c
CaiYiLiang <cherryliang0112@gmail.com>
2020-10-16 13:54:13 +1300

    add pokablocks-FE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState, createRef } from 'react';
-import { Container, Dimmer, Loader, Grid, Sticky, Message } from 'semantic-ui-react';
+import {
+  Container,
+  Dimmer,
+  Loader,
+  Grid,
+  Sticky,
+  Message,
+} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 import { SubstrateContextProvider, useSubstrate } from './substrate-lib';
@@ -16,7 +23,7 @@ import TemplateModule from './TemplateModule';
 import Transfer from './Transfer';
 import Upgrade from './Upgrade';
 
-function Main () {
+function Main() {
   const [accountAddress, setAccountAddress] = useState(null);
   const { apiState, keyring, keyringState, apiError } = useSubstrate();
   const accountPair =
@@ -24,26 +31,33 @@ function Main () {
     keyringState === 'READY' &&
     keyring.getPair(accountAddress);
 
-  const loader = text =>
+  const loader = (text) => (
     <Dimmer active>
       <Loader size='small'>{text}</Loader>
-    </Dimmer>;
+    </Dimmer>
+  );
 
-  const message = err =>
+  const message = (err) => (
     <Grid centered columns={2} padded>
       <Grid.Column>
-        <Message negative compact floating
+        <Message
+          negative
+          compact
+          floating
           header='Error Connecting to Substrate'
           content={`${err}`}
         />
       </Grid.Column>
-    </Grid>;
+    </Grid>
+  );
 
   if (apiState === 'ERROR') return message(apiError);
   else if (apiState !== 'READY') return loader('Connecting to Substrate');
 
   if (keyringState !== 'READY') {
-    return loader('Loading accounts (please review any extension\'s authorization)');
+    return loader(
+      "Loading accounts (please review any extension's authorization)"
+    );
   }
 
   const contextRef = createRef();
@@ -61,10 +75,10 @@ function Main () {
             <BlockNumber />
             <BlockNumber finalized />
           </Grid.Row>
-          <Grid.Row stretched>
+          {/* <Grid.Row stretched>
             <Balances />
-          </Grid.Row>
-          <Grid.Row>
+          </Grid.Row> */}
+          {/* <Grid.Row>
             <Transfer accountPair={accountPair} />
             <Upgrade accountPair={accountPair} />
           </Grid.Row>
@@ -74,7 +88,7 @@ function Main () {
           </Grid.Row>
           <Grid.Row>
             <TemplateModule accountPair={accountPair} />
-          </Grid.Row>
+          </Grid.Row> */}
         </Grid>
       </Container>
       <DeveloperConsole />
@@ -82,7 +96,7 @@ function Main () {
   );
 }
 
-export default function App () {
+export default function App() {
   return (
     <SubstrateContextProvider>
       <Main />
